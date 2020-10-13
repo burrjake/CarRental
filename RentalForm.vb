268d44721500b79268a8b761f73ec6687880dc53
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-10-13 08:56:27 -0600

    Add project files.

---
+++ RentalForm.vb
@@ -0,0 +1,10 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Binary
+Public Class RentalForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+
+End Class
