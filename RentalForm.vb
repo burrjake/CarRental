2c6af84dad76230aecc594c49082e76ff1ade470
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-10-15 08:55:31 -0600

    change title of quit message box to exit

--- RentalForm.vb
+++ RentalForm.vb
@@ -3,7 +3,7 @@ Option Strict On
 Option Compare Binary
 Public Class RentalForm
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
-        Dim msgNumber = MsgBox("Are you sure you want to Quit?", MsgBoxStyle.YesNo, "Quit")
+        Dim msgNumber = MsgBox("Are you sure you want to Quit?", MsgBoxStyle.YesNo, "Exit")
         If msgNumber = 6 Then
             Me.Close()
         Else
