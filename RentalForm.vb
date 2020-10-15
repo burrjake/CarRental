bc384a9ee36efadeba5375720f32e07b5e4c1efb
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-10-15 08:53:46 -0600

    add do you want to quit? message box

--- RentalForm.vb
+++ RentalForm.vb
@@ -3,7 +3,11 @@ Option Strict On
 Option Compare Binary
 Public Class RentalForm
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
-        Me.Close()
+        Dim msgNumber = MsgBox("Are you sure you want to Quit?", MsgBoxStyle.YesNo, "Quit")
+        If msgNumber = 6 Then
+            Me.Close()
+        Else
+        End If
     End Sub
 
 
